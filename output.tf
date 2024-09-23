3db656ebecc15dda8c222b177ff18398f84b4882
vijay-saw <vijaysaw50@gmail.com>
2024-09-23 23:38:41 +0530

    output.tf changes

--- output.tf
+++ output.tf
@@ -1,15 +1,24 @@
-output "websiteendpoint" {
-    value=aws_s3_bucket.vijaybucket.website_endpoint
-  
+# Output for the S3 Website Endpoint
+output "website_endpoint" {
+  value       = aws_s3_bucket.vijaybucket.website_endpoint
 }
 
-output "ec2ami" {
-    value = aws_instance.nginxserver
-  
+# Output for the Nginx Server EC2 Instance ID
+output "nginx_ec2_instance_id" {
+  value       = aws_instance.nginxserver.id
 }
 
-output "ec2ami2" {
-    value = aws_instance.terraforminstance
-  
+# Output for the Static Web Hosting EC2 Instance ID
+output "static_web_ec2_instance_id" {
+  value       = aws_instance.terraforminstance.id
 }
 
+# Output for the Nginx Server Public IP
+output "nginx_server_public_ip" {
+  value       = aws_instance.nginxserver.public_ip
+}
+
+# Output for the Static Web Hosting Public IP
+output "static_web_public_ip" {
+  value       = aws_instance.terraforminstance.public_ip
+}
